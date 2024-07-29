6429277728816508f2c979455a0e43c13209a95b
Rodolfo Ilce Pereira <rodolfo.pereira@g.globo>
2024-07-29 14:48:52 -0300

    feat: preact added into the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rodolfo-astro-tutorial.netlify.app/"
+  site: "https://rodolfo-astro-tutorial.netlify.app/",
+  integrations: [preact()]
 });
